74b613931ab5d12c43ef1c71b6eb56546df8779a
pasupuletiteja <raviteja355.p@gmail.com>
2025-10-24 11:26:21 +0530

    new

--- test3.sh
+++ test3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-person1=ravi
-person2=teja
+person1=$1
+person2=$2
 
 
-echo "$person1::  hi $person2"
-echo "$person2::  hi $person1"
+echo "$person1::  hi $person2 , this is $person1"
+echo "$person2::  hello $person1, how are youi"
